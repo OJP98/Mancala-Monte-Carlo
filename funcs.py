49adfda718a20aaadc8baa93ca17939eede3dbb8
josepablito16 <josepablocif16@gmail.com>
2020-10-10 15:17:35 -0600

    The main is finish, win condition and PC turn

--- funcs.py
+++ funcs.py
@@ -1,3 +1,153 @@
+
+import random
+import copy
+import os
+import time
+# ! *********************************************
+
+
+def getNextMove(board):
+    posicionesDisponibles = []
+    for i in range(0, 6):
+        if(board[i] != 0):
+            posicionesDisponibles.append(i)
+    return posicionesDisponibles[int(random.uniform(0, len(posicionesDisponibles)))]
+
+
+def getPosiblesTurnos(board):
+    posicionesDisponibles = []
+    for i in range(0, 6):
+        if(board[i] != 0):
+            posicionesDisponibles.append(i)
+    return posicionesDisponibles
+
+
+def isEnd(board):
+    control1 = False
+    control2 = False
+
+    if(board[0][0] == board[0][1] == board[0][2] == board[0][3] == board[0][4] == board[0][5] == 0):
+        control1 = True
+    if(board[1][0] == board[1][1] == board[1][2] == board[1][3] == board[1][4] == board[1][5] == 0):
+        control2 = True
+
+    return control1 or control2
+
+
+def getPoints(board):
+    points1 = board[0][0] + board[0][1] + board[0][2] + \
+        board[0][3] + board[0][4] + board[0][5] + board[0][6]
+
+    points2 = board[1][0] + board[1][1] + board[1][2] + \
+        board[1][3] + board[1][4] + board[1][5] + board[1][6]
+
+    return points1, points2
+    #print(f'Puntos jugador 0 {points1}\nPuntos jugador 1 {points2}')
+
+
+def monteCarloMove(board):
+    jugadorActual = 1
+    control = False
+    firstMove = None
+    while(not isEnd(board)):
+        # ? Jugador 0 Humano
+        # ? Jugador 1 Monte Carlo
+
+        if(jugadorActual == 1):
+            # * Tira jugador 1
+            nextMove = getNextMove(board[1])
+            board, jugadorActual = Move(
+                playerTurn=1, cell=nextMove, board=board)
+
+            if (not control):
+                firstMove = nextMove
+                control = True
+
+        else:
+            # * Tira jugador 0
+            board, jugadorActual = Move(
+                playerTurn=0, cell=getNextMove(board[0]), board=board)
+
+    points1, points2 = getPoints(board)
+
+    if(points2 > points1):
+        # Gano
+        return firstMove, 1
+    else:
+        # perdio
+        return firstMove, 0
+
+
+def monteCarloBestMove(originalBoard, iterations):
+    movesDict = {}
+    for i in range(0, 6):
+        movesDict[i] = [0, 0]  # Wins, Total
+
+    for _ in range(iterations):
+        iterationMove, winLose = monteCarloMove(copy.deepcopy(originalBoard))
+        if(iterationMove != None):
+            movesDict[iterationMove][1] += 1
+
+            if(winLose):
+                movesDict[iterationMove][0] += 1
+
+    for i in range(0, 6):
+        try:
+            movesDict[i] = movesDict[i][0]/movesDict[i][1]
+        except:
+            movesDict[i] = 0
+
+    movesDict = dict(
+        sorted(movesDict.items(), key=lambda x: x[1], reverse=True))
+
+    return list(movesDict.keys())[0]
+
+
+def getNumIteration(BORRAR):
+    while True:
+        print("Select the difficulty level")
+        print("1. Noob")
+        print("2. Advanced")
+        print("3. Pro")
+        selection = input("> ")
+
+        try:
+            if(1 <= int(selection) <= 3):
+                if(int(selection) == 1):
+                    return 0
+                if(int(selection) == 2):
+                    return 500
+                else:
+                    return 10000
+            else:
+                print("Enter a valid option!")
+                time.sleep(2)
+                os.system(BORRAR)
+
+        except:
+            print("Enter a valid option!")
+            time.sleep(2)
+            os.system(BORRAR)
+
+
+def validateUserTurn(board):
+    userTurn = input("> ")
+    posibleTurns = getPosiblesTurnos(board)
+    try:
+        if(int(userTurn) in posibleTurns):
+            return int(userTurn)
+        else:
+            print("Enter a valid option!")
+            time.sleep(1)
+            return None
+    except:
+        print("Enter a valid option!")
+        time.sleep(1)
+        return None
+
+# ! *********************************************
+
+
 def Move(playerTurn, cell, board):
     lastPos = 0
     currRow = playerTurn
@@ -40,8 +190,7 @@ def Move(playerTurn, cell, board):
             # We just add 1 stone to the current cell
             board[currRow][cell] = board[currRow][cell] + 1
             stones -= 1
-        
-    
+
     #                     CHECK IF LAST POS == 1
     # If that's the case, we take the stones in the front row.
     #################################################################
@@ -57,7 +206,6 @@ def Move(playerTurn, cell, board):
         # If the other player has stones in its cell, collect them
         if frontStones > 0:
             board[currRow][-1] = board[currRow][-1] + frontStones
-
 
     #                  CHECK IF LAST POS == PIT
     #################################################################
--- main.py
+++ main.py
@@ -2,6 +2,9 @@
 from funcs import *
 import random
 import copy
+import platform
+import os
+import time
 
 # 0 for player 1 / 1 for player 2
 score = [0, 0]
@@ -14,115 +17,58 @@ actualBoard = [
     [4, 4, 4, 4, 4, 4, 0]
 ]
 
-# ! *********************************************
+if(platform.system() == 'Windows'):
+    BORRAR = 'cls'
+else:
+    BORRAR = 'clear'
 
 
-def getNextMove(board):
-    posicionesDisponibles = []
-    for i in range(0, 6):
-        if(board[i] != 0):
-            posicionesDisponibles.append(i)
-    return posicionesDisponibles[int(random.uniform(0, len(posicionesDisponibles)))]
+os.system(BORRAR)
+NUM_ITERATIONS = getNumIteration(BORRAR)
 
 
-def isEnd(board):
-    control1 = False
-    control2 = False
+while(not isEnd(actualBoard)):
+    os.system(BORRAR)
+    PrintBoard(actualBoard)
 
-    if(board[0][0] == board[0][1] == board[0][2] == board[0][3] == board[0][4] == board[0][5] == 0):
-        control1 = True
-    if(board[1][0] == board[1][1] == board[1][2] == board[1][3] == board[1][4] == board[1][5] == 0):
-        control2 = True
+    if(playerTurn == 0):
+        while 1:
+            userTurn = validateUserTurn(copy.deepcopy(actualBoard[0]))
+            if(userTurn == None):
+                os.system(BORRAR)
+                PrintBoard(actualBoard)
+                continue
+            else:
+                actualBoard, playerTurn = Move(
+                    playerTurn=0, cell=0, board=copy.deepcopy(actualBoard))
+                break
 
-    return control1 or control2
-
-
-def getPoints(board):
-    points1 = board[0][0] + board[0][1] + board[0][2] + \
-        board[0][3] + board[0][4] + board[0][5] + board[0][6]
-
-    points2 = board[1][0] + board[1][1] + board[1][2] + \
-        board[1][3] + board[1][4] + board[1][5] + board[1][6]
-
-    return points1, points2
-    #print(f'Puntos jugador 0 {points1}\nPuntos jugador 1 {points2}')
-
-
-def monteCarloMove(board):
-    jugadorActual = 1
-    control = False
-    firstMove = None
-    while(not isEnd(board)):
-        # ? Jugador 0 Humano
-        # ? Jugador 1 Monte Carlo
-
-        if(jugadorActual == 1):
-            # * Tira jugador 1
-            nextMove = getNextMove(board[1])
-            board, jugadorActual = Move(
-                playerTurn=1, cell=nextMove, board=board)
-
-            if (not control):
-                firstMove = nextMove
-                control = True
-
-        else:
-            # * Tira jugador 0
-            board, jugadorActual = Move(
-                playerTurn=0, cell=getNextMove(board[0]), board=board)
-
-    points1, points2 = getPoints(board)
-
-    if(points2 > points1):
-        # Gano
-        return firstMove, 1
     else:
-        # perdio
-        return firstMove, 0
+        print("PC turn...")
+        pcTurn = monteCarloBestMove(copy.deepcopy(actualBoard), NUM_ITERATIONS)
+        actualBoard, playerTurn = Move(
+            playerTurn=1, cell=pcTurn, board=copy.deepcopy(actualBoard))
+        time.sleep(1)
 
+os.system(BORRAR)
+PrintBoard(actualBoard)
+points1, points2 = getPoints(actualBoard)
+print()
 
-def monteCarloBestMove(originalBoard, iterations):
-    movesDict = {}
-    for i in range(0, 6):
-        movesDict[i] = [0, 0]  # Wins, Total
+if(points1 > points2):
+    print("The winner is Player 1")
+else:
+    print("The winner is Player 2")
 
-    for _ in range(iterations):
-        iterationMove, winLose = monteCarloMove(copy.deepcopy(originalBoard))
-        if(iterationMove != None):
-            movesDict[iterationMove][1] += 1
-
-            if(winLose):
-                movesDict[iterationMove][0] += 1
-
-    for i in range(0, 6):
-        try:
-            movesDict[i] = movesDict[i][0]/movesDict[i][1]
-        except:
-            movesDict[i] = 0
-
-    movesDict = dict(
-        sorted(movesDict.items(), key=lambda x: x[1], reverse=True))
-
-    return list(movesDict.keys())[0]
-
-    # ! *********************************************
-
-
+'''
 print("Tablero Inicial")
 PrintBoard(actualBoard)
 print()
-'''
 
-board, nextPlayerTurn = Move(playerTurn=0, cell=5, board=board)
-print("Tablero")
-print(board)
+actualBoard, nextPlayerTurn = Move(
+	playerTurn=1, cell=5, board=copy.deepcopy(actualBoard))
 
-print()
-'''
-print("Mejor Tiro")
-# Posicion 0 del board siempre es mi tablero
-# playerId 0 siempre para mi
-print(monteCarloBestMove(copy.deepcopy(actualBoard), 10000))
 
 print()
 PrintBoard(actualBoard)
+'''
