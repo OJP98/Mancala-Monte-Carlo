01415fb373e7b78525644f8331ce135b9a6b4056
OJP98 <ojuarez_p@hotmail.com>
2020-10-11 20:49:37 -0600

    Board formatted and cleaned code

--- funcs.py
+++ funcs.py
@@ -224,7 +224,15 @@ def Move(playerTurn, cell, board):
 def PrintBoard(board):
     boardFormat = f'''
     {board[1][5]}  {board[1][4]}  {board[1][3]}  {board[1][2]}  {board[1][1]}  {board[1][0]}
-{board[1][6]}                       {board[0][6]}
+{board[1][6]}\t\t\t{board[0][6]}
     {board[0][0]}  {board[0][1]}  {board[0][2]}  {board[0][3]}  {board[0][4]}  {board[0][5]}
+--------------------------
+    1  2  3  4  5  6
+
+==========================
+
+SCORE
+You: {board[0][6]}
+PC: {board[1][6]}
     '''
     print(boardFormat)
--- main.py
+++ main.py
@@ -27,7 +27,7 @@ while not isEnd(actualBoard):
     PrintBoard(actualBoard)
 
     if playerTurn == 0:
-        print("Your turn...")
+        print("Your turn... Select a cell")
         while 1:
             userTurn = validateUserTurn(copy.deepcopy(actualBoard[0]))
             if userTurn == None:
@@ -39,31 +39,17 @@ while not isEnd(actualBoard):
                 break
 
     else:
-        print("PC turn...")
+        print("PC's turn...")
         pcTurn = monteCarloBestMove(copy.deepcopy(actualBoard), NUM_ITERATIONS)
         actualBoard, playerTurn = Move(
             playerTurn=1, cell=pcTurn, board=copy.deepcopy(actualBoard))
         time.sleep(1)
 
-os.system(BORRAR)
 PrintBoard(actualBoard)
 points1, points2 = getPoints(actualBoard)
 print()
 
 if(points1 > points2):
-    print("The winner is Player 1")
+    print("You win!")
 else:
-    print("The winner is Player 2")
-
-'''
-print("Tablero Inicial")
-PrintBoard(actualBoard)
-print()
-
-actualBoard, nextPlayerTurn = Move(
-	playerTurn=1, cell=5, board=copy.deepcopy(actualBoard))
-
-
-print()
-PrintBoard(actualBoard)
-'''
+    print("PC wins!")
